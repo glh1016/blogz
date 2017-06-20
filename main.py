e154ddec7bf103947e0d244052fa2e4555a3fdd8
Garrett Haas <garrett.l.haas@gmail.com>
2017-06-20 09:00:03 -0500

    working on templates and establishing db

--- main.py
+++ main.py
@@ -3,18 +3,50 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    body = db.Column(db.String(1000))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
 @app.route('/', methods=['POST', 'GET'])
-def index():
+def index()
+
+    return render_template('mainblog.html')
+
+
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def add_post():
+     
+      if request.method == 'POST':
+        blog_title = request.form['title']
+        new_title = Task(blog_title)
+        blog_body = request.form['body']
+        new_body = Task(blog_body)
+        db.session.add(new_title)
+        db.session.add(new_body)
+        db.session.commit()
+     
+     
+     return render_template('newpost.html', title = "Add a Blog", )
 
 
 
 
 
 
-
+if __name__ == '__main__':
 app.run()
