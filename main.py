950b8e4419d5e9dcc7ebaba3999bba454f25c5c9
Garrett Haas <garrett.l.haas@gmail.com>
2017-06-23 02:12:54 -0500

    need to be able to submit new entries

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -32,22 +32,6 @@ class User(db.Model):
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
-
-
-@app.route('/', methods=['POST','GET'])
-def landing():
-    id = request.args.get('id')
-
-    if id:
-        post = Blog.query.filter_by(id=id).first()
-        return render_template('indblogview.html',post=post)
-   
-    posts = Blog.query.all()
-    return render_template('mainblog.html',posts=posts)
-
-
-
 
 
 @app.route('/blog', methods=['POST','GET'])
@@ -97,6 +81,16 @@ def add_post():
     return render_template('newpost.html')
 
 
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup', 'index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+
+
+
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
 
@@ -105,13 +99,11 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
-            #session['username'] = username
-            #flash("Logged in")
+            session['username'] = username
+            flash("Logged in")
             return redirect('/')
-        else:
-            pass
-            #return '<h1> error </h1>' 
-            #flash('User password incorrect, or user does not exist', 'error')
+        else: 
+            flash('User password incorrect, or username does not exist', 'error')
 
     return render_template('login.html')
 
@@ -119,7 +111,8 @@ def login():
 @app.route('/logout')
 def logout():
     del session['username']
-    return redirect('/')
+    flash("Logged out")
+    return redirect('/login')
 
 
 
@@ -145,7 +138,26 @@ def signup():
             return "<h1>Duplicate user</h1>"
 
     return render_template('signup.html')
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        blog_body = request.form['body']
+        owner= User.query.filter_by(username=session['username']).first()
+        new_blog_entry = Blog(blog_title,blog_body,owner)
+        db.session.add(new_blog_entry)
+        db.session.commit()
+
+    return render_template('index.html')
     
+    
+
+
+
+
 
 
 
@@ -156,8 +168,4 @@ if __name__ == '__main__':
 
 
 
-#@app.route('/newpost', methods=['POST', 'GET'])
-#def index():
-
-#logout
 
